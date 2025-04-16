b4315a0f837950f4f655cd5fd37de8811cc461bd
mmostafa.c <mostafatarekaioup@gmail.com>
2025-04-16 13:15:33 +0200

    learn signals and conditional attributes and @if & @for in angular

--- src/app/test-com-sec/test-com-sec.component.ts
+++ src/app/test-com-sec/test-com-sec.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { TestComComponent } from '../test-com/test-com.component';
 
 @Component({
@@ -8,4 +8,26 @@ import { TestComComponent } from '../test-com/test-com.component';
   templateUrl: './test-com-sec.component.html',
   styleUrl: './test-com-sec.component.scss',
 })
-export class TestComSecComponent {}
+export class TestComSecComponent {
+  firstName = signal('Morgan');
+  isDisabled = signal(false);
+  namesList = signal(['Morgan', 'Darsh', 'Raj', 'Rahul']);
+  constructor() {
+    console.log('wwwwwww', this.firstName());
+
+    // computed used to produce values based on other signals
+    const firstNameCapitalized = computed(() => this.firstName().toUpperCase());
+    console.log(firstNameCapitalized());
+    console.log(this.firstName());
+    // whenever  signal changes the value of computed signal changes too
+    this.firstName.set('Darsh co');
+    console.log(firstNameCapitalized());
+  }
+  displayName = (name: string): void => {
+    console.log('ssssssssssssss', this.firstName(), name, this.namesList());
+  };
+
+  disableButton = (): void => {
+    this.isDisabled.set(!this.isDisabled());
+  };
+}
