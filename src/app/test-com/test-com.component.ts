bf2685eda4c2805c86e82cb3c2be59277aa7b23d
mmostafa.c <mostafatarekaioup@gmail.com>
2025-04-29 09:20:09 +0300

    learn to add services

--- src/app/test-com/test-com.component.ts
+++ src/app/test-com/test-com.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Calculator } from '../../services/calculator';
 
 @Component({
   selector: 'app-test-com',
-  standalone: true, // Add standalone: true
-
-  imports: [],
+  standalone: true,
+  // Make sure the Calculator service is provided here for standalone components
+  providers: [Calculator],
   templateUrl: './test-com.component.html',
   styleUrl: './test-com.component.scss',
 })
-export class TestComComponent {}
+export class TestComComponent {
+  private calc = inject(Calculator);
+  totalCost = this.calc.add(50, 10);
+
+  constructor() {
+    console.log('wwwwwwwwwwwwwwwwwwwwwwwwwwwwwww333');
+    console.log('🚀 ~ TestComComponent ~ totalCost:', this.totalCost);
+  }
+}
